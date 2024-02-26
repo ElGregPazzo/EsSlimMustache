84eab92c167bfddc882bfae3a96640abc17c9ee9
ElGregPazzo <128700266+ElGregPazzo@users.noreply.github.com>
2024-02-26 11:21:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ include __DIR__ .'/controllers/AlunniController.php';
 $app = AppFactory::create();
 
 $app->get('/alunni', 'AlunniController:index'); 
-    
+$app->get('/alunni/{nome_alunno}', 'AlunniController:show');
+$app->get('/json/alunni', 'AlunniController:json_alunni'); 
+$app->get('/json/alunni/{nome}', 'AlunniController:json_alunni'); 
+
 
 $app->run();
+
